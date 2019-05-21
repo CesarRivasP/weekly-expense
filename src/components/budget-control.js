7832aa3ca1040bef727634bebfedfeb73029c4e5
CesarRivasP <cesarjose58@gmail.com>
2019-05-21 17:17:20 -0400

    change for functional component

--- src/components/budget-control.js
+++ src/components/budget-control.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Budget from './budget';
 import Remaining from './remaining';
 
-class BudgetControl extends Component {
-  render(){
-    return (
-      <React.Fragment>
-        <Budget budget={this.props.budget} />
-        <Remaining remaining={this.props.remaining}/>
-      </React.Fragment>
-    );
-  }
+const BudgetControl = (props) => {
+  return (
+    <React.Fragment>
+      <Budget budget={props.budget} />
+      <Remaining budget={props.budget} remaining={props.remaining}/>
+    </React.Fragment>
+  )
 }
 
 export default BudgetControl;
