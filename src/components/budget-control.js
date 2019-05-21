f73140a8a9786b7370773d8af475e2686eb6d722
CesarRivasP <cesarjose58@gmail.com>
2019-05-21 16:19:50 -0400

    Create BudgetControl component

---
+++ src/components/budget-control.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class BudgetControl extends Component {
+  render(){
+    return (
+      <p>BudgetControl</p>
+    );
+  }
+}
+
+export default BudgetControl;
