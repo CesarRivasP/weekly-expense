2f0b35ce4ad66eea0ef84f2d8f1065fbee071ab0
CesarRivasP <cesarjose58@gmail.com>
2019-05-21 12:09:44 -0400

    IMplemented handleSpending function and create references

--- src/components/form.js
+++ src/components/form.js
@@ -13,19 +13,41 @@ const styles = theme => ({
 });
 
 class Form extends Component {
+  constructor(props){
+    super(props)
+    this.state = {}
+    this.spendingNameRef = React.createRef();
+    this.quantitySpendingRef = React.createRef();
+  }
+
+  handleSpendings = (e) => {
+    // prevenir default
+    e.preventDefault(); //evita que se recargue la pagina al realizar submit
+    // crear el objeto con los datos
+    const spending = {
+      spendingName: this.spendingNameRef.current.value,
+      quantitySpending: this.quantitySpendingRef.current.value
+    }
+    console.log(spending);
+    // agregarlo y enviarlo por props
+
+    // resetear el formulario
+    e.currentTarget.reset();
+  }
+
   render(){
     const { classes } = this.props;
     return (
-      <form>
+      <form onSubmit={this.handleSpendings}>
         <h2>Agrega tus gastos</h2>
 
         <div className="campo">
           <label>Nombre de gasto</label>
-          <input type="text" className="u-full-width" placeholder="Ejemplo: Transporte" />
+          <input type="text" className="u-full-width" placeholder="Ejemplo: Transporte" ref={this.spendingNameRef} />
         </div>
         <div className="campo">
           <label>Cantidad</label>
-          <input type="text" className="u-full-width" placeholder="Ejemplo: 300" />
+          <input type="text" className="u-full-width" placeholder="Ejemplo: 300" ref={this.quantitySpendingRef} />
         </div>
 
         <Button type="submit" value="add" variant="outlined" color="primary" className={classes.button}>
