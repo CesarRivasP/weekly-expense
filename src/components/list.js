3279badb5a9b7cd1f2923a00636815b093a7a75b
CesarRivasP <cesarjose58@gmail.com>
2019-05-21 15:19:17 -0400

    Render new spendings

--- src/components/list.js
+++ src/components/list.js
@@ -1,11 +1,22 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import Spending from './spending';
 
 class List extends Component {
   render(){
+    const { spendings } = this.props;
     return(
-      <p>desde listado</p>
+      <div className="gastos-realizados">
+        <h2>Listado</h2>
+        {/* Se obtienen las llaves de los objetos, para despues mapear uno a uno esos objetos mediante su llave */}
+        {Object.keys(spendings).map((key) => (
+          <Spending spendings={spendings[key]} key={key} />
+          // {/*asi se selecciona el key actual*/}
+        ))}
+      </div>
     )
   }
 }
 
 export default List;
+// Object.keys para iterar un objeto
+// map para acceder a dicho objeto
