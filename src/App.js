a854e5e99fbbdbd44905d24408a472f9e4afc6cd
CesarRivasP <cesarjose58@gmail.com>
2019-05-21 10:50:35 -0400

    Implemented header app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+import Header from './components/header';
 import './css/App.css';
 
 class App extends Component {
   render(){
     return (
-      <div className="App">
-        <header className="App-header">
-        </header>
+      <div className="App container">
+        <Header title="Gasto Semanal" />
+        <div className="contenido-principal contenido">
+          <div className="row">
+            <div className="one-half column">
+
+            </div>
+            <div className="one-half column">
+
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
